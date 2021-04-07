6d2de58f3dd1c199bcf278abc08fee3d48b1e1e2
Adam Synnott <adam.synnott@theiconic.com.au>
2021-04-07 10:15:56 +1000

    Removed example code from main file

--- engineering/typescript-interview/index.ts
+++ engineering/typescript-interview/index.ts
@@ -8,7 +8,6 @@ const client = axios.create({
 
 async function run(): Promise<void> {    
     const productApi = new ProductApi(client);
-
     /*
         Your Challenge Starts here
         Write a script that consumes our product API and decorates results with video preview links 
@@ -18,18 +17,7 @@ async function run(): Promise<void> {
         * Think about how you might approach this problem if you had 100 products or 100,000 products?
     */
 
-    const output = [];
-
-    // Example get products with a page size - please delete this line if you like
-    const products = await productApi.getProducts({ page_size: 10, page: 1 });
-
-    // Example how to get a video preview for a Product - please delete this line if you like
-    const videoPreview = await productApi.getVideoPreviewForProduct(products[0]);
-
-    // ...
-    
     // log the result to the console
-    console.log(output);
 }
 
 run();
